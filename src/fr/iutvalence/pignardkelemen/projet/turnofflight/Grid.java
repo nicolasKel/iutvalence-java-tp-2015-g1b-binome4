9519f59120df993438d02e348f4784ef0780a6c0
Pignard <florianpignard@gmail.com>
2015-05-02 17:23:19 +0200

    Creat method initlevel with 8 switch for 8 levels.

--- src/fr/iutvalence/pignardkelemen/projet/turnofflight/Grid.java
+++ src/fr/iutvalence/pignardkelemen/projet/turnofflight/Grid.java
@@ -82,6 +82,131 @@ public class Grid
 		}
 	}
 
+	private void initlevel()
+	{
+		int level = 1;
+
+		for (int line = 0; line < 9; line++)
+		{
+			for (int column = 0; column < 9; column++)
+			{
+				switch (level)
+				{
+				case 1:
+					grid[line][column] = new Lamp(State.OFF, new Position(line, column));
+					if (line == 8 && column == 8)
+					{
+						grid[4][4] = new Lamp(State.ON, new Position(line, column));
+						level = level + 1;
+					}
+					break;
+				case 2:
+					grid[line][column] = new Lamp(State.OFF, new Position(line, column));
+					if (line == 8 && column == 8)
+					{
+						grid[3][4] = new Lamp(State.ON, new Position(line, column));
+						grid[4][3] = new Lamp(State.ON, new Position(line, column));
+						grid[5][4] = new Lamp(State.ON, new Position(line, column));
+						grid[4][5] = new Lamp(State.ON, new Position(line, column));
+						level = level + 1;
+					}
+					break;
+				case 3:
+					grid[line][column] = new Lamp(State.OFF, new Position(line, column));
+					if (line == 8 && column == 8)
+					{
+						grid[3][4] = new Lamp(State.ON, new Position(line, column));
+						grid[4][3] = new Lamp(State.ON, new Position(line, column));
+						grid[5][4] = new Lamp(State.ON, new Position(line, column));
+						grid[4][5] = new Lamp(State.ON, new Position(line, column));
+						grid[1][1] = new Lamp(State.ON, new Position(line, column));
+						grid[1][6] = new Lamp(State.ON, new Position(line, column));
+						grid[6][1] = new Lamp(State.ON, new Position(line, column));
+						grid[6][6] = new Lamp(State.ON, new Position(line, column));
+						level = level + 1;
+					}
+					break;
+				case 4:
+					grid[line][column] = new Lamp(State.OFF, new Position(line, column));
+					if (line == 8 && column == 8)
+					{
+						grid[0][0] = new Lamp(State.ON, new Position(line, column));
+						grid[0][7] = new Lamp(State.ON, new Position(line, column));
+						grid[7][0] = new Lamp(State.ON, new Position(line, column));
+						grid[7][7] = new Lamp(State.ON, new Position(line, column));
+						grid[6][6] = new Lamp(State.ON, new Position(line, column));
+						grid[5][5] = new Lamp(State.ON, new Position(line, column));
+						grid[4][4] = new Lamp(State.ON, new Position(line, column));
+						grid[3][3] = new Lamp(State.ON, new Position(line, column));
+						grid[2][2] = new Lamp(State.ON, new Position(line, column));
+						grid[1][1] = new Lamp(State.ON, new Position(line, column));
+						level = level + 1;
+					}
+					break;
+				case 5:
+					grid[line][column] = new Lamp(State.OFF, new Position(line, column));
+					if (line == 8 && column == 8)
+					{
+						grid[4][0] = new Lamp(State.ON, new Position(line, column));
+						grid[4][1] = new Lamp(State.ON, new Position(line, column));
+						grid[4][2] = new Lamp(State.ON, new Position(line, column));
+						grid[4][3] = new Lamp(State.ON, new Position(line, column));
+						grid[4][4] = new Lamp(State.ON, new Position(line, column));
+						grid[4][5] = new Lamp(State.ON, new Position(line, column));
+						grid[4][6] = new Lamp(State.ON, new Position(line, column));
+						grid[4][7] = new Lamp(State.ON, new Position(line, column));
+						level = level + 1;
+					}
+					break;
+				case 6:
+					grid[line][column] = new Lamp(State.OFF, new Position(line, column));
+					if (line == 8 && column == 8)
+					{
+						grid[4][0] = new Lamp(State.ON, new Position(line, column));
+						grid[4][1] = new Lamp(State.ON, new Position(line, column));
+						grid[4][2] = new Lamp(State.ON, new Position(line, column));
+						grid[4][3] = new Lamp(State.ON, new Position(line, column));
+						grid[4][4] = new Lamp(State.ON, new Position(line, column));
+						grid[4][5] = new Lamp(State.ON, new Position(line, column));
+						grid[4][6] = new Lamp(State.ON, new Position(line, column));
+						grid[4][7] = new Lamp(State.ON, new Position(line, column));
+						level = level + 1;
+					}
+					break;
+				case 7:
+					grid[line][column] = new Lamp(State.OFF, new Position(line, column));
+					if (line == 8 && column == 8)
+					{
+						grid[2][4] = new Lamp(State.ON, new Position(line, column));
+						grid[3][3] = new Lamp(State.ON, new Position(line, column));
+						grid[4][2] = new Lamp(State.ON, new Position(line, column));
+						grid[5][3] = new Lamp(State.ON, new Position(line, column));
+						grid[6][4] = new Lamp(State.ON, new Position(line, column));
+						grid[5][5] = new Lamp(State.ON, new Position(line, column));
+						grid[4][6] = new Lamp(State.ON, new Position(line, column));
+						grid[3][5] = new Lamp(State.ON, new Position(line, column));
+						level = level + 1;
+					}
+					break;
+				case 8:
+					grid[line][column] = new Lamp(State.ON, new Position(line, column));
+					if (line == 8 && column == 8)
+					{
+						level = level + 1;
+					}
+					break;
+				default:
+					grid[line][column] = new Lamp(State.OFF, new Position(line, column));
+					if (line == 8 && column == 8)
+					{
+						grid[4][4] = new Lamp(State.ON, new Position(line, column));
+					}
+					break;
+				}
+			}
+		}
+	}
+
 	public void swap(Position position)
 	{
 		this.grid[position.getLine()][position.getColumn()].swap();
