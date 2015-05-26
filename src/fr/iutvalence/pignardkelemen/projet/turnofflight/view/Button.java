823baf1f7e21cc369b4dc9b2db80a5cda809e227
Falcon004 <florianpignard@gmail.com>
2015-05-26 18:37:55 +0200

    Modification of class Button and MainWindow.

--- src/fr/iutvalence/pignardkelemen/projet/turnofflight/view/Button.java
+++ src/fr/iutvalence/pignardkelemen/projet/turnofflight/view/Button.java
@@ -1,18 +1,43 @@
 package fr.iutvalence.pignardkelemen.projet.turnofflight.view;
 
 import javax.swing.JButton;
+import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.IOException; 
+import javax.imageio.ImageIO
+
 
 import fr.iutvalence.pignardkelemen.projet.turnofflight.controler.PositionInterface;
 import fr.iutvalence.pignardkelemen.projet.turnofflight.model.Position;
+package fr.iutvalence.pignardkelemen.projet.turnofflight.model.Grid;
 
-public class Button extends JButton
+public class Button extends JButton implements MouseListener
 {
-	Position pos;
+	private Position pos;
+	private Image image;
 
 	public Button(Position pos)
 	{
 		super();
 		this.pos = pos;
-
+		try
+		{
+			img = ImageIO.read(new File("ampoule.png"));
+		{
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		this.addMouseListener(this);
+		
+	}
+	
+	public void mouseClicked(MouseEvent event)
+	{
+		Grid.swap(pos);
 	}
 }
+
--- src/fr/iutvalence/pignardkelemen/projet/turnofflight/view/MainWindow.java
+++ src/fr/iutvalence/pignardkelemen/projet/turnofflight/view/MainWindow.java
@@ -1,6 +1,7 @@
 package fr.iutvalence.pignardkelemen.projet.turnofflight.view;
 
 import java.awt.GridLayout;
+import java.awt.Color;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -14,12 +15,24 @@ public class MainWindow extends JFrame
 	public final static int HEIGHT = 600;
 	public final static String TITLE = "Turn off light";
 
+	public Bouton bouton = new Bouton();
+	public JPanel pannel = new JPanel();
+	public JLabel title = new JLabel(TITLE);
+
 	public MainWindow(int i, int j)
 	{
 		super(TITLE);
 		this.setSize(WIDTH, HEIGHT);
-		JPanel pannel = new JPanel();
-		pannel.setLayout(new GridLayout(4, 4));
+		setLocationRelativeTo(null);
+		setResizable(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		Font typeface = new Font("Arial", Font.BOLD, 8);
+		title.setFont(typeface);
+		title.setForeground(Color.blue);
+		
+		pannel.setBackground(Color.white);
+		pannel.setLayout(new GridLayout(4, 4, 3, 3));
 		for (int line = 0; line < i; line++)
 		{
 			for (int column = 0; column < j; column++)
